33bc454e092f008cbb95e3fff6f7142e734cb59d
xichen1 <xichen.pan@gmail.com>
2020-10-18 00:09:59 -0600

    Created User class

--- Code/app/src/main/java/com/example/bookit/ProfileFragment.java
+++ Code/app/src/main/java/com/example/bookit/ProfileFragment.java
@@ -1,8 +1,10 @@
 package com.example.bookit;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -10,7 +12,16 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        User testUser = new User("testName", "testUser", "testID",
+                "testEmail", 123456, "123456abc");
+
+        TextView userNameView = v.findViewById(R.id.userName);
+        TextView contactInfoView = v.findViewById(R.id.contactInfo);
+
+        userNameView.setText(testUser.getUserName());
+        contactInfoView.setText(Integer.toString(testUser.getContactInfo()));
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return v;
     }
 }
