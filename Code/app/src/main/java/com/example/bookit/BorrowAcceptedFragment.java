dcadd763b1646fb315a6b7eeebdcf1da7dbf5d76
zhengyao603 <zhengyao@ualberta.ca>
2020-10-25 08:00:07 -0600

    finish interface of 4 subpages of borrowing page

--- Code/app/src/main/java/com/example/bookit/BorrowAcceptedFragment.java
+++ Code/app/src/main/java/com/example/bookit/BorrowAcceptedFragment.java
@@ -1,15 +1,16 @@
 package com.example.bookit;
+
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
-public class BorrowFragment extends Fragment {
-    private Button acceptedButton;
+public class BorrowAcceptedFragment extends Fragment {
     private Button availableButton;
     private Button borrowedButton;
     private Button requestedButton;
@@ -17,26 +18,17 @@ public class BorrowFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_borrow, container, false);
+        final View view = inflater.inflate(R.layout.fragment_borrow_accepted, container, false);
         // get view of four buttons
-        acceptedButton = view.findViewById(R.id.button_accepted);
         availableButton = view.findViewById(R.id.button_available);
         borrowedButton = view.findViewById(R.id.button_borrowed);
         requestedButton = view.findViewById(R.id.button_requested);
-
-        acceptedButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // switch to MyBookAcceptedFragment
-                Navigation.findNavController(view).navigate(R.id.action_borrow_to_borrow_accepted);
-            }
-        });
 
         availableButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // switch to MyBookAvailableFragment
-                Navigation.findNavController(view).navigate(R.id.action_borrow_to_borrow_available);
+                Navigation.findNavController(view).navigate(R.id.action_borrow_accepted_to_borrow_available);
             }
         });
 
@@ -44,7 +36,7 @@ public class BorrowFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 // switch to MyBookBorrowedFragment
-                Navigation.findNavController(view).navigate(R.id.action_borrow_to_borrow_borrowed);
+                Navigation.findNavController(view).navigate(R.id.action_borrow_accepted_to_borrow_borrowed);
             }
         });
 
@@ -52,7 +44,7 @@ public class BorrowFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 //switch to MyBookRequestedFragment
-                Navigation.findNavController(view).navigate(R.id.action_borrow_to_borrow_requested);
+                Navigation.findNavController(view).navigate(R.id.action_borrow_accepted_to_borrow_requested);
             }
         });
 
