9aa1b59cca1eb394b82fba2b856882d8c9eef66a
zhengyao603 <zhengyao@ualberta.ca>
2020-10-25 01:24:47 -0600

    update skeleton of MyBookFragment

--- Code/app/src/main/java/com/example/bookit/MyBookFragment.java
+++ Code/app/src/main/java/com/example/bookit/MyBookFragment.java
@@ -3,14 +3,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class MyBookFragment extends Fragment {
+    private Button acceptedButton;
+    private Button availableButton;
+    private Button borrowedButton;
+    private Button requestedButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mybook, container, false);
+        View view = inflater.inflate(R.layout.fragment_mybook, container, false);
+        // get view of four buttons
+        acceptedButton = view.findViewById(R.id.button_accepted);
+        availableButton = view.findViewById(R.id.button_available);
+        borrowedButton = view.findViewById(R.id.button_borrowed);
+        requestedButton = view.findViewById(R.id.button_requested);
+
+        acceptedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // switch to MyBookAcceptedFragment
+            }
+        });
+
+        availableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // switch to MyBookAvailableFragment
+            }
+        });
+
+        borrowedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // switch to MyBookBorrowedFragment
+            }
+        });
+
+        requestedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //switch to MyBookRequestedFragment
+            }
+        });
+
+        return view;
     }
 }
