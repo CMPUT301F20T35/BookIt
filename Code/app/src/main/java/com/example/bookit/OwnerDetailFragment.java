042bbef4d320bd98cfa34cad40d47585cb1a7587
SHAWNXU <72234348+shawnxxxu@users.noreply.github.com>
2020-10-26 17:52:26 +0800

    Create OwnerDetailFragment.java

---
+++ Code/app/src/main/java/com/example/bookit/OwnerDetailFragment.java
@@ -0,0 +1,18 @@
+package com.example.bookit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+public class OwnerDetailFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_owner_detail, container, false);
+        return view;
+    }
+}
