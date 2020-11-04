7999f36acaf2ce101c773215f16ee315736d62d7
SHAWNXU <72234348+shawnxxxu@users.noreply.github.com>
2020-11-04 18:33:15 +0800

    Update fragment_requestList.java

--- Code/app/src/main/java/com/example/bookit/fragment_requestList.java
+++ Code/app/src/main/java/com/example/bookit/fragment_requestList.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +23,7 @@ public class fragment_requestList extends Fragment {
 
     RecyclerView rv;
     UserAdapter uAdapter;
-    private Button backButton;
+    private ImageView backButton;
     private TextView ownerDetail;
 
     @Override
