0c4d076d870ec9f493b8863645efd4ac015ce664
SHAWNXU <72234348+shawnxxxu@users.noreply.github.com>
2020-11-07 01:12:58 +0800

    Create US090101Test.java

---
+++ Code/app/src/androidTest/java/com/example/bookit/US090101Test.java
@@ -0,0 +1,62 @@
+package com.example.bookit;
+
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class US090101Test {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<Login> rule =
+            new ActivityTestRule<Login>(Login.class, true, true);
+
+    @Before
+    public void setUp() {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    @Test
+    public void Test() {
+        solo.assertCurrentActivity("Wrong Activity", Login.class);
+
+        // login with test account
+        solo.enterText((EditText) solo.getView(R.id.emailText), "test@gmail.com");
+        solo.waitForText("test@gmail.com", 1, 1000);
+        solo.enterText((EditText) solo.getView(R.id.password), "test123456");
+        solo.waitForText("test123456", 1, 1000);
+        solo.clickOnButton("Login");
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        solo.clickOnView(solo.getView(R.id.navigation_my_book));
+        solo.clickOnButton("Requested");
+        solo.waitForFragmentByTag("MyBookRequestedFragment", 5000);
+        solo.clickInRecyclerView(0);
+        solo.waitForFragmentByTag("fragment_requestList", 5000);
+        solo.clickOnView(solo.getView(R.id.accpet));
+        solo.waitForFragmentByTag("fragment_maps", 5000);
+        solo.clickOnView(solo.getView(R.id.confirm));
+        solo.waitForFragmentByTag("fragment_requestList", 5000);
+        solo.clickOnButton("Accepted");
+        solo.waitForFragmentByTag("fragment_acceptedList", 5000);
+
+
+
+    }
+
+    @After
+    public void tearDown() {
+        solo.finishOpenedActivities();
+    }
+}
