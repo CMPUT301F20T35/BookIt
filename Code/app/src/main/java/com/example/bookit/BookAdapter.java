a57ca3e14677cd2cbcf0f200deab27a78cd4aea4
xichen1 <xichen.pan@gmail.com>
2020-11-23 09:41:47 -0700

    show owner name in list

--- Code/app/src/main/java/com/example/bookit/BookAdapter.java
+++ Code/app/src/main/java/com/example/bookit/BookAdapter.java
@@ -1,6 +1,7 @@
 package com.example.bookit;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -47,6 +48,7 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.LinearViewHold
     public void onBindViewHolder(@NonNull BookAdapter.LinearViewHolder holder, final int position) {
         holder.bookName.setText(bookData.get(position).getTitle());
         holder.authorName.setText(bookData.get(position).getAuthor());
+        holder.ownerName.setText("owner: " + bookData.get(position).getOwnerName());
         //holder.state.setText(bookData.get(position).getISBN());
         fs=new FireStoreHelper(mContext);
         fs.fetch_MyBookState(bookData.get(position).getISBN(), new dbCallback() {
@@ -54,6 +56,18 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.LinearViewHold
             public void onCallback(Map map) {
                 String state = map.get("state").toString();
                 holder.state.setText(state);
+                switch (state) {
+                    case "REQUESTED": holder.state.setTextColor(Color.parseColor("#F44336"));
+                        break;
+                    case "BORROWED": holder.state.setTextColor(Color.parseColor("#FF9800"));
+                        break;
+                    case "ACCEPTED": holder.state.setTextColor(Color.parseColor("#4CAF50"));
+                        break;
+                    case "AVAILABLE": holder.state.setTextColor(Color.parseColor("#3F51B5"));
+                        break;
+                    default: holder.state.setTextColor(Color.parseColor("#fff"));
+                }
+
             }
         });
 
@@ -95,12 +109,14 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.LinearViewHold
         private TextView bookName;
         private TextView authorName;
         private TextView state;
+        private TextView ownerName;
 
         public LinearViewHolder(View itemView){
             super(itemView);
             bookName = itemView.findViewById(R.id.book_name);
             authorName = itemView.findViewById(R.id.author);
             state = itemView.findViewById(R.id.status);
+            ownerName = itemView.findViewById(R.id.row_render_owner);
         }
     }
 
